3a471d410be712665d520f4ec1fc920159756284
praneshbk <59330065+praneshbk@users.noreply.github.com>
2020-02-28 13:18:13 +0530

    project files
    added all project files

---
+++ adminpannel/admin/includes/footer.php
@@ -0,0 +1,4 @@
+
+                <footer class="footer text-right" style="position: fixed; bottom: 0; left: 0;">
+                Copyright &copy; News portal Designed & Developed by Akshay Narake and Pranesh Kudari
+                </footer>
